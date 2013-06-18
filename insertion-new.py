990fbbc0738b12477d0ab6878acf340c984dbce0
kiran9k <kiran9k99@gmail.com>
2013-06-18 09:27:01 +0530

    second commit

---
+++ insertion-new.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+def insert_elements():
+    a=[]
+    print "enter the numbers :"
+    print "-1 is the escape character"
+    while(1):
+        x=int(raw_input())
+        if(x== -1):
+            break
+        a.append(x)
+    return(a)
+
+def insertion(a):
+    N=len(a)
+    for i in range(N):
+        min=i
+        for j in  range(i,N):
+            if (a[j]<a[min]):
+                min=j
+
+        print a[min]
+        temp=a[min]
+        
+        for k in range(min,i,-1):
+            a[k]=a[k-1]
+        a[i]=temp    
+        
+        
+        print a
+            
+            
+    return a
+
+if(__name__=='__main__'):
+    print "insertion sort:"
+    
+    x=insert_elements()
+    print "the unsorted elements are:"
+    print x
+    y=insertion(x)
+    print"\nthe sorted elements are: "
+    print y
+    
+
